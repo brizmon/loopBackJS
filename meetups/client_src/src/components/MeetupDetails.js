3c1a1629cc00e445cbcaea2857c0fdf2b5b30f5e
Briseida Montiel <brizism@gmail.com>
2017-10-28 23:22:27 -0400

    fetch meetup details

--- meetups/client_src/src/components/MeetupDetails.js
+++ meetups/client_src/src/components/MeetupDetails.js
@@ -9,12 +9,35 @@ class MeetupDetails extends Component{
             details: ''
         }
     }
+
+    componentWillMount(){
+        this.getMeetup();
+    }
+
+    getMeetup(){
+        let meetupId = this.props.match.params.id;
+        axios.get(`http://localhost:3000/api/meetups/${meetupId}`)
+        .then(response => {
+            this.setState({details: response.data}, () => {
+                console.log(this.state)
+            })
+        })
+        .catch(err => console.log(err));
+    }
+
     render(){
         return(
             <div>
                 <br />
                 <Link className="btn grey" to="/">Back</Link>
-                <h1>Details</h1>
+                <h1>{this.state.details.name}</h1>
+                <ul className="collection">
+                    <li className="collection-item">City: {this.state.details.city}</li>
+                    <li className="collection-item">Address: {this.state.details.address}</li>
+                </ul>
+                <Link className="btn" to={`/meetups/edit/${this.state.details.id}`}>Edit</Link>
+
+                <button className="btn red right">Delete</button>
             </div>
         )
     }
