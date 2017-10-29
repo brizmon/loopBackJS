3dd3484e9c6c61b565673fc89e20bfd67ee38bf2
Briseida Montiel <brizism@gmail.com>
2017-10-28 23:02:26 -0400

    create meetup details component

---
+++ meetups/client_src/src/components/MeetupDetails.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class MeetupDetails extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            details: ''
+        }
+    }
+    render(){
+        return(
+            <div>
+                <br />
+                <Link className="btn grey" to="/">Back</Link>
+                <h1>Details</h1>
+            </div>
+        )
+    }
+}
+
+export default MeetupDetails;
