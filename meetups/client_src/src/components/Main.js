44b9f2033120459ffd839ef9eb3f41d138cc4b64
Briseida Montiel <brizism@gmail.com>
2017-11-02 20:37:12 -0400

    add route to add meetups to main

--- meetups/client_src/src/components/Main.js
+++ meetups/client_src/src/components/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Meetups from './Meetups';
 import MeetupDetails from './MeetupDetails';
+import AddMeetup from './AddMeetup';
 import About from './About';
 
 const Main = () => (
@@ -9,6 +10,7 @@ const Main = () => (
         <Switch>
             <Route exact path='/' component={Meetups} />
             <Route exact path='/about' component={About} />
+            <Route exact path='/meetups/add' component={AddMeetup} />
             <Route exact path='/meetups/:id' component={MeetupDetails} />
         </Switch>
     </main>
