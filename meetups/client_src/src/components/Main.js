f7676f197fceddddadc42099e813ee5a8c1147fe
Briseida Montiel <brizism@gmail.com>
2017-11-02 22:09:25 -0400

    route to edit meetup in main

--- meetups/client_src/src/components/Main.js
+++ meetups/client_src/src/components/Main.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Meetups from './Meetups';
 import MeetupDetails from './MeetupDetails';
 import AddMeetup from './AddMeetup';
+import EditMeetup from './EditMeetup';
 import About from './About';
 
 const Main = () => (
@@ -11,6 +12,7 @@ const Main = () => (
             <Route exact path='/' component={Meetups} />
             <Route exact path='/about' component={About} />
             <Route exact path='/meetups/add' component={AddMeetup} />
+            <Route exact path='/meetups/edit/:id' component={EditMeetup} />
             <Route exact path='/meetups/:id' component={MeetupDetails} />
         </Switch>
     </main>
